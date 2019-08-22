0aa0ee6a0e6f75bd23238465ff263c61f0e085e3
Alexander Koller <koller@ling.uni-potsdam.de>
2019-08-22 09:29:26 +0200

    asynchronous messages

--- src/main/java/de/saar/minecraft/matchmaker/Matchmaker.java
+++ src/main/java/de/saar/minecraft/matchmaker/Matchmaker.java
@@ -20,7 +20,6 @@ public class Matchmaker {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
-                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
                 Matchmaker.this.stop();
                 System.err.println("*** server shut down");
@@ -46,14 +45,55 @@ public class Matchmaker {
     }
 
     private static class MatchmakerImpl extends MatchmakerGrpc.MatchmakerImplBase {
+        /**
+         * Handles the start of a game. Creates a record for this game in the database
+         * and returns a unique game ID to the client.
+         *
+         * @param request
+         * @param responseObserver
+         */
         @Override
         public void startGame(GameData request, StreamObserver<GameId> responseObserver) {
             int id = nextGameId++;
 
-            System.err.printf("start game %s -> id %d\n", request.getGameIdentifier(), id);
+            System.err.printf("start game %s -> id %d\n", request.getGameData(), id);
             GameId idMessage = GameId.newBuilder().setId(id).build();
             responseObserver.onNext(idMessage);
             responseObserver.onCompleted();
+        }
+
+        /**
+         * Handles a status update from the Minecraft server. Optionally, sends back a TextMessage
+         * with a string that is to be displayed to the user. Because calculating this text message
+         * may take a long time, this method should be called asynchronously (with a non-blocking stub).
+         *
+         * @param request
+         * @param responseObserver
+         */
+        @Override
+        public void handleStatusInformation(StatusMessage request, StreamObserver<TextMessage> responseObserver) {
+            int x = request.getX();
+            int gameId = request.getGameId();
+
+            // spawn a thread for a long-running computation
+            new Thread() {
+                @Override
+                public void run() {
+                    String text = "your x was " + x;
+                    TextMessage mText = TextMessage.newBuilder().setGameId(gameId).setText(text).build();
+
+                    // delay for a bit
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // send the text message back to the client
+                    responseObserver.onNext(mText);
+                    responseObserver.onCompleted();
+                }
+            }.start();
         }
     }
 
--- src/main/java/de/saar/minecraft/matchmaker/MatchmakerTestClient.java
+++ src/main/java/de/saar/minecraft/matchmaker/MatchmakerTestClient.java
@@ -1,17 +1,29 @@
 package de.saar.minecraft.matchmaker;
 
-import com.google.protobuf.TextFormat;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A test client for the matchmaker. This is a mockup class that generates
+ * messages that would usually be sent by the Minecraft server. You can enter
+ * the following kinds of strings on the console. Type "status id" to send
+ * a status message for game id "id", and receive a text message back asynchronously.
+ * Type any other string to create a new game with the given game data.
+ * Type Ctrl-D to quit the client.
+ *
+ */
 public class MatchmakerTestClient {
     private ManagedChannel channel;
     private MatchmakerGrpc.MatchmakerBlockingStub blockingStub;
+    private MatchmakerGrpc.MatchmakerStub nonblockingStub;
 
-    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    /**
+     * Construct client connecting to HelloWorld server at {@code host:port}.
+     */
     public MatchmakerTestClient(String host, int port) {
         this(ManagedChannelBuilder.forAddress(host, port)
                 // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
@@ -20,18 +32,27 @@ public class MatchmakerTestClient {
                 .build());
     }
 
-    /** Construct client for accessing HelloWorld server using the existing channel. */
+    /**
+     * Construct client for accessing HelloWorld server using the existing channel.
+     */
     MatchmakerTestClient(ManagedChannel channel) {
         this.channel = channel;
         blockingStub = MatchmakerGrpc.newBlockingStub(channel);
+        nonblockingStub = MatchmakerGrpc.newStub(channel);
     }
 
     public void shutdown() throws InterruptedException {
         channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
-    public int registerGame(String gameIdentifier) {
-        GameData mGameInfo = GameData.newBuilder().setGameIdentifier(gameIdentifier).build();
+    /**
+     * Registers a game with the matchmaker. Returns a unique game ID for this game.
+     *
+     * @param gameData
+     * @return
+     */
+    public int registerGame(String gameData) {
+        GameData mGameInfo = GameData.newBuilder().setGameData(gameData).build();
         GameId mGameId;
         try {
             mGameId = blockingStub.startGame(mGameInfo);
@@ -43,17 +64,57 @@ public class MatchmakerTestClient {
         return mGameId.getId();
     }
 
+    /**
+     * Sends a status message for the given game ID to the matchmaker. Handles
+     * any text messages that the matchmaker sends back.
+     *
+     * @param gameId
+     * @param x
+     * @param y
+     * @param z
+     */
+    public void sendStatusMessage(int gameId, int x, int y, int z) {
+        StatusMessage mStatus = StatusMessage.newBuilder().setGameId(gameId).setX(x).setY(y).setZ(z).build();
+
+        nonblockingStub.handleStatusInformation(mStatus, new StreamObserver<TextMessage>() {
+            @Override
+            public void onNext(TextMessage value) {
+                System.err.printf("got text message for gameid %d: %s\n", gameId, value);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+            }
+
+            @Override
+            public void onCompleted() {
+            }
+        });
+
+    }
+
     public static void main(String[] args) throws InterruptedException {
         MatchmakerTestClient client = new MatchmakerTestClient("localhost", 2802);
 
         try {
-            System.out.print("enter game data: ");
-            String gameId = System.console().readLine();
+            while (true) {
+                System.out.print("enter game data: ");
+                String gameData = System.console().readLine();
 
-            int playerId = client.registerGame(gameId);
-            System.err.printf("got game ID %d\n", playerId);
+                if (gameData == null) {
+                    break;
+                } else if (gameData.startsWith(STATUS)) {
+                    int id = Integer.parseInt(gameData.substring(STATUS.length() + 1));
+                    client.sendStatusMessage(id, 1, 2, 3);
+                } else {
+                    int playerId = client.registerGame(gameData);
+                    System.err.printf("got game ID %d\n", playerId);
+                }
+            }
         } finally {
             client.shutdown();
         }
     }
+
+    private static final String STATUS = "status";
 }
